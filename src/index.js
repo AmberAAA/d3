e95af62f58b28406dc582984b6e345945ef45515
AmberAAA <qe-d@163.com>
2019-01-14 21:08:16 +0800

    ...

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import * as d3 from "d3";
-import "./main.js"
+import "./index.css";
+import "./index.scss";
 
 d3.select("body")
 	.selectAll("p")
-	.data([4, 8, 15, 16, 23, 42])
+	.data([123123123123, 8, 15, 123, 2333, 123])
 	.enter().append("p")
 	.text(function(d) { return "I’m number " + d + "!"; });
