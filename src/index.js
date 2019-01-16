74543e0a3b81e155ac9c020bf39205a9f19cf099
AmberAAA <qe-d@163.com>
2019-01-16 11:33:49 +0800

    add editorconfig  & eslint

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 import * as d3 from "d3";
-import "./index.css";
-import "./index.scss";
 
-d3.select("body")
-	.selectAll("p")
-	.data([123123123123, 8, 15, 123, 2333, 123])
-	.enter().append("p")
-	.text(function(d) { return "I’m number " + d + "!"; });
+d3.select("p");
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import * as d3 from "d3";
-
-d3.select("body")
-	.selectAll("p")
-	.data([1,2,3,4,5])
-	.enter().append("p")
-	.text(function(d) { return "I’m number " + d + "!"; });
