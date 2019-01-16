49f5dde3ede2dd5eebf4d6526e0e8901cdb3a117
AmberAAA <qe-d@163.com>
2019-01-16 14:41:51 +0800

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import * as d3 from "d3";
 
-d3.select("p");
+/* eslint-disable-next-line */
+console.log(d3.version);
+
+d3.selectAll("p")
+    .data([4, 8, 15, 16, 23, 42])
+    .style("font-size", (d) => `${d}px`)
+    .enter()
+    .append("p")
+    .text(d => d)
+    .style("font-size", (d) => `${d}px`);
